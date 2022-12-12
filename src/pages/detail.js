706e911ec88b23ce6e9e3e29e706a7653d1ded2d
Swhag <khg5293@outlook.com>
2022-12-12 02:02:20 -0500

    Details module update

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Details(props) {
   let { id } = useParams();
   let currentShoes = props.shoes.find((x) => x.id === parseInt(id));
+  let [alert, setAlert] = useState(true);
+
+  let [count, setCount] = useState(60);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 600000);
+
+    setTimeout(() => {
+      setCount(count - 1);
+    }, 1000);
+  });
 
   return (
     <div className='container'>
+      {alert ? (
+        <div className='alert alert-warning'>
+          20% holiday discount ends in {count} seconds
+        </div>
+      ) : null}
+
       <div className='row'>
         <div className='col-md-6'>
           <img src={props.shoesImage[id]} alt='#' width='100%' />
