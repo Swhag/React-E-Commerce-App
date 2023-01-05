38e3eeca67b808a3e955ddbeaf0255d9aea61f35
Swhag <khg5293@outlook.com>
2023-01-05 02:48:09 -0500

    pages drop down animation fixed

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -12,7 +12,11 @@ function HeroSlider() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img className='d-block w-100' alt='First slide' src={slideImages[0]} />
+        <img
+          className='d-block w-100 slide-images'
+          alt='First slide'
+          src={slideImages[0]}
+        />
         <Carousel.Caption>
           <h3>Step Into The Cool</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -20,7 +24,7 @@ function HeroSlider() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className='d-block w-100'
+          className='d-block w-100 slide-images'
           src={slideImages[1]}
           alt='Second slide'
         />
@@ -31,7 +35,11 @@ function HeroSlider() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className='d-block w-100' src={slideImages[2]} alt='Third slide' />
+        <img
+          className='d-block w-100 slide-images'
+          src={slideImages[2]}
+          alt='Third slide'
+        />
 
         <Carousel.Caption>
           <h3>Discover the Style Difference</h3>
