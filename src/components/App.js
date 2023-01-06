49e4fc28809d6493dc40652fffcf005e014d709e
Swhag <khg5293@outlook.com>
2023-01-06 00:54:26 -0500

    imports fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate, Link } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 
